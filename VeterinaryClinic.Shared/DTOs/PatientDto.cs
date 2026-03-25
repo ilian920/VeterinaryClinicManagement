4b6cb4f071b8fa0715f81e7757bec297c0b6ab99
ilian920 <109132372+ilian920@users.noreply.github.com>
2026-03-25 15:07:37 +0200

    Add PatientDto class

---
+++ VeterinaryClinic.Shared/DTOs/PatientDto.cs
@@ -0,0 +1,9 @@
+public class PatientDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Breed { get; set; }
+    public string OwnerName { get; set; }
+    public string OwnerContact { get; set; }
+}
