0a8159a98fc0397784f0969bbbb143a387091a9e
ilian920 <109132372+ilian920@users.noreply.github.com>
2026-03-25 15:06:57 +0200

    Add VeterinaryClinicContext DbContext configuration

---
+++ VeterinaryClinic.Data/VeterinaryClinicContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using VeterinaryClinic.Data.Entities;
+
+namespace VeterinaryClinic.Data
+{
+    public class VeterinaryClinicContext : DbContext
+    {
+        public VeterinaryClinicContext(DbContextOptions<VeterinaryClinicContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        // Add other DbSets for your entities here
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure entity properties and relationships
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
