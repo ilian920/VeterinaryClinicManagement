cfc602583d97fec818159ebda956569e357ab399
ilian920 <109132372+ilian920@users.noreply.github.com>
2026-03-25 16:36:26 +0200

    Add User DTO

---
+++ VeterinaryClinic.Shared/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+// UserDto.cs
+
+namespace VeterinaryClinic.Shared.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
